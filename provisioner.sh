8e47beede0015f74dd8ce9a553719b34a6dd4f59
Alexander Six <alexanderhsix@gmail.com>
2017-03-27 00:14:33 -0400

    Adds a lot of lesson files for PHP basics

--- provisioner.sh
+++ provisioner.sh
@@ -18,7 +18,7 @@ server {
 	listen 80 default_server;
 	listen [::]:80 default_server ipv6only=on;
 
-	root /vagrant;
+	root /vagrant/phpTest;
 	index index.php index.html index.htm;
 
 	server_name server_domain_or_IP;
@@ -29,7 +29,7 @@ server {
 
 	location ~ \.php\$ {
 		try_files \$uri /index.php =404;
-		fastcgi_split_path_info ^(.+\.php) (/.+) \$;
+		fastcgi_split_path_info ^(.+\.php)(/.+)\$;
 		fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
 		fastcgi_index index.php;
 		fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
